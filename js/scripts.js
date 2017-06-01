ce03b7a732785d56743957ca4e65180b0846e2e8
Lukasz Ludwin <gorli@interia.pl>
2017-06-01 12:38:40 +0200

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+﻿var femaleNames = ["Asia", "Basia", "Kasia", "Ola", "Jola"];
+var maleNames = ["Jan", "Franek", "Kazek", "Arek", "Bronek"];
+var allNames = femaleNames.concat(maleNames);
+var newName = "Gerwazy";
+if (allNames.indexOf(newName) === -1) {
+	allNames.push(newName);
+}
+
+console.log("cała tablica: " + allNames.toString());
